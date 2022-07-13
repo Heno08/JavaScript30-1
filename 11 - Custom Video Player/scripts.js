073fb5ffff3fe7c3406b5fcd33e5de2af411d56f
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-07-13 12:09:07 +0200

    added fullscreen btn

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreenBtn = player.querySelector('.fullscreen');
 
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
@@ -35,6 +36,10 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function fullscreen() {
+  video.requestFullscreen()
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
@@ -50,3 +55,4 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+fullscreenBtn.addEventListener('click', fullscreen);
